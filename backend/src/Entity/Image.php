68d731af5a794235f1db95c9c32bbf0b55a2fb8b
deunoh <denovann@live.fr>
2024-08-05 14:59:39 +0200

    install symfony flex and vich uploader bundle, also db has been created

---
+++ backend/src/Entity/Image.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ImageRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ImageRepository::class)]
+class Image
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $link = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $device = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLink(): ?string
+    {
+        return $this->link;
+    }
+
+    public function setLink(string $link): static
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+
+    public function getDevice(): ?string
+    {
+        return $this->device;
+    }
+
+    public function setDevice(?string $device): static
+    {
+        $this->device = $device;
+
+        return $this;
+    }
+}
